95ae3e8a329d7b7b160f26bf44504162bd468a05
Ramadhan <dev.ramddd@gmail.com>
2023-09-17 23:56:19 +0700

    simple express api

--- Belajar Dasar Pemrograman JavaScript/index.js
+++ Belajar Dasar Pemrograman JavaScript/index.js
@@ -1,3 +1,16 @@
-console.log("Hello World!");
+const express = require("express");
 
-// Tampilkan nama Anda ke layar pada kode di bawah:
+// This line of code will be interpreted as part of the previous statement
+app = express;
+
+const port = 3000;
+
+// Define a route
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
