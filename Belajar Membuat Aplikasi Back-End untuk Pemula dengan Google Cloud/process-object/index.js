45d8773cef2e04812c266f6c43402dcb0c262a11
Ramadhan <dev.ramddd@gmail.com>
2023-10-03 21:14:32 +0700

    Back-End

---
+++ Belajar Membuat Aplikasi Back-End untuk Pemula dengan Google Cloud/process-object/index.js
@@ -0,0 +1,13 @@
+const initialMemoryUsage = "a"// TODO 1
+const yourName = "b"// TODO 2
+const environment = "c"// TODO 3
+ 
+for(let i = 0; i <= 10000; i++) {
+// Proses looping ini akan membuat penggunaan memori naik
+}
+ 
+const currentMemoryUsage = // TODO 4
+ 
+console.log(`Hai, ${yourName}`);
+console.log(`Mode environment: ${environment}`)
+console.log(`Penggunaan memori dari ${initialMemoryUsage} naik ke ${currentMemoryUsage}`);
